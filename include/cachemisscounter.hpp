201afd4b6767cf359d1628bc3daa911340ac4c65
shubhajeet <shubhajeet.per@gmail.com>
2023-04-09 20:04:44 -0500

    Adding cache miss counter

---
+++ include/cachemisscounter.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+class CacheMissCounter
+{
+public:
+    CacheMissCounter();
+    ~CacheMissCounter();
+
+    void start();
+    void stop();
+
+    uint64_t get_cache_misses() const;
+
+private:
+    int fd_;
+    uint64_t cache_misses_;
+};
+
+class AvgCacheMissCounter
+{
+public:
+    AvgCacheMissCounter(const std::string name);
+    ~AvgCacheMissCounter();
+
+    void start();
+    void stop();
+
+    double get_avg_cache_misses() const;
+
+private:
+    std::string name_;
+    CacheMissCounter cache_miss_counter_;
+    uint64_t total_cache_misses_;
+    uint64_t num_samples_;
+};
---
+++ src/cachemisscounter.cpp
@@ -0,0 +1,88 @@
+#include "cachemisscounter.hpp"
+
+#include <linux/perf_event.h>
+#include <asm/unistd.h>
+#include <cstdio>
+#include <cstring>
+#include <stdexcept>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <iostream>
+
+CacheMissCounter::CacheMissCounter()
+    : fd_(-1), cache_misses_(0)
+{
+    struct perf_event_attr attr;
+    memset(&attr, 0, sizeof(attr));
+    attr.type = PERF_TYPE_HW_CACHE;
+    attr.config = PERF_COUNT_HW_CACHE_MISSES |
+                  (PERF_COUNT_HW_CACHE_L1D << 16) |
+                  (PERF_COUNT_HW_CACHE_OP_READ << 24) |
+                  (PERF_COUNT_HW_CACHE_RESULT_MISS << 0);
+    attr.size = sizeof(attr);
+    attr.disabled = 1;
+    attr.exclude_kernel = 1;
+    attr.exclude_hv = 1;
+    fd_ = syscall(__NR_perf_event_open, &attr, 0, -1, -1, 0);
+    if (fd_ == -1)
+    {
+        throw std::runtime_error("perf_event_open() failed");
+    }
+}
+
+CacheMissCounter::~CacheMissCounter()
+{
+    close(fd_);
+}
+
+void CacheMissCounter::start()
+{
+    ioctl(fd_, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd_, PERF_EVENT_IOC_ENABLE, 0);
+}
+
+void CacheMissCounter::stop()
+{
+    ioctl(fd_, PERF_EVENT_IOC_DISABLE, 0);
+    ssize_t ret = read(fd_, &cache_misses_, sizeof(cache_misses_));
+    if (ret != sizeof(cache_misses_))
+    {
+        throw std::runtime_error("read() failed");
+    }
+}
+
+uint64_t CacheMissCounter::get_cache_misses() const
+{
+    return cache_misses_;
+}
+
+AvgCacheMissCounter::AvgCacheMissCounter(const std::string name)
+    : total_cache_misses_(0), num_samples_(0), name_(name)
+{
+}
+
+AvgCacheMissCounter::~AvgCacheMissCounter()
+{
+    std::cout << "Instrumentation CodeBlock: " << name_ << " total_count: " << total_cache_misses_ << " average_cache_misses: " << get_avg_cache_misses() << std::endl;
+}
+
+void AvgCacheMissCounter::start()
+{
+    cache_miss_counter_.start();
+}
+
+void AvgCacheMissCounter::stop()
+{
+    cache_miss_counter_.stop();
+    total_cache_misses_ += cache_miss_counter_.get_cache_misses();
+    num_samples_++;
+}
+
+double AvgCacheMissCounter::get_avg_cache_misses() const
+{
+    if (num_samples_ == 0)
+    {
+        return 0.0;
+    }
+    return static_cast<double>(total_cache_misses_) / static_cast<double>(num_samples_);
+}
